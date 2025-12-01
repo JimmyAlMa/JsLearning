c08c0125dce404520fdf8145317b6bd1ff96c63c
Jimmy Al <qanbelong@gmail.com>
2025-12-01 16:21:51 +0700

    Day2

--- workspace.js
+++ workspace.js
@@ -1,9 +1,45 @@
-const myCatName = 'Ciko';
-const myDogName = 'Dug';
-console.log('Aku punya dua hewan peliharaan, ' + myCatName + ' si kucing dan ' + myDogName + ' si anjing.');
+// Boolean
 
-const catEat = 2;
-const dogEat = 3;
-const totalEat = catEat + dogEat;
-console.log('Ciko makan apel ' + catEat + ' dan Dug makan ' + dogEat);
-console.log('Total mereka makan apel ada ' + totalEat)
+const isTall = true;
+const canCook = true;
+const hasSiblings = true;
+
+// Conditional Statements
+
+const isSoldOut = false;
+if (isSoldOut) {
+  console.log("Produk sudah habis");
+} else {
+  console.log("Produk masih tersedia");
+}
+
+if (2 !== 1) {
+    console.log('Benar')
+} else {
+    console.log('Salah')
+}
+
+// comparison operators
+
+console.log(10 > 5); // Besar dari
+console.log(8 < 4); // Kecil dari
+console.log(-1 >= 2); // Besar dari atau sama dengan
+console.log(0.5 <= 0); // Kecil dari atau sama dengan
+console.log('abc' === 'abc'); // Sama dengan
+console.log(false !== true); // Tidak sama dengan
+console.log('   ')
+console.log('cab' === 'cba');
+console.log(false !== false);
+console.log('   ');
+// else if statement
+
+const value = 52562;
+if (value < 10) {
+    console.log('Satuan');
+} else if (value < 100) {
+    console.log('Puluhan');
+} else if (value < 1000) {
+    console.log('Ratusan')
+} else {
+    console.log(value);
+}
