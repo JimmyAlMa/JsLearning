973b0a9467f6fe2c8382e19064d34ae28086f680
Jimmy Al <qanbelong@gmail.com>
2025-12-01 15:16:26 +0700

    Day1

---
+++ workspace.js
@@ -0,0 +1,9 @@
+const myCatName = 'Ciko';
+const myDogName = 'Dug';
+console.log('Aku punya dua hewan peliharaan, ' + myCatName + ' si kucing dan ' + myDogName + ' si anjing.');
+
+const catEat = 2;
+const dogEat = 3;
+const totalEat = catEat + dogEat;
+console.log('Ciko makan apel ' + catEat + ' dan Dug makan ' + dogEat);
+console.log('Total mereka makan apel ada ' + totalEat)
